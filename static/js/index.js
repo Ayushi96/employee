f9ea2eff168ce8e82588328b47ce7e205bde2d4f
Ayushi Vishwakarma <cs17mtech11007@iith.ac.in>
2021-05-31 00:14:34 +0530

    code cleanup

--- static/js/index.js
+++ static/js/index.js
@@ -1,43 +1,27 @@
-console.log("included js file of employee ")
-
-const url = 'emp_data.json'
 const flaskapi_url = 'http://127.0.0.1:5000/fetchEmployee/'
 
 async function loadData(url) {
-    console.log(url)
     const response = await fetch(url)
-    console.log("response is ", response)
     let data = await response.json();
-    // console.log("data within loadData function ", data );
     return data;
 }
 
 async function fetchEmployee(id){
-    console.log("called fetchEmployee()");
     const url_emp = flaskapi_url + id
     data = await loadData(url_emp)
     console.log("data from the backend is => ", data);
-    let firstName = data.firstName;
-    let lastName = data.lastName;
-    let dob = data.dob;
-    let hireDate = data.hireDate;
-    let department = data.department;
-    let designation = data.designation;
-    let salary = data.salary;
-    let manager = data.manager;
-    document.getElementById("firstName").value = firstName;
-    document.getElementById("lastName").value = lastName;
-    document.getElementById("DOB").value = dob;
-    document.getElementById("hireDate").value = hireDate;
-    document.getElementById("department").value = department;
-    document.getElementById("salary").value = salary;
-    document.getElementById("manager").value = manager;
-    document.getElementById("designation").value = designation;
+    document.getElementById("firstName").value = data.firstName;
+    document.getElementById("lastName").value = data.lastName;
+    document.getElementById("DOB").value = data.dob;
+    document.getElementById("hireDate").value = data.hireDate;
+    document.getElementById("department").value = data.department;
+    document.getElementById("salary").value = data.salary;
+    document.getElementById("manager").value = data.manager;
+    document.getElementById("designation").value = data.designation;
 
 }
 
 function handleSearch(){
-    console.log("calling the new handle search")
     const empId = document.getElementById('empId').value;
     console.log("empID = ", typeof(empId));
     if(empId < 1 || empId > 3 || empId == ''){
@@ -56,8 +40,3 @@ function handleSearch(){
     fetchEmployee(empId);
 
 }
-
-
-
-
-
